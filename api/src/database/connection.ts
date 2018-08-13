201da86fb2c8e856d4ee6f758121641e7ed07ec0
lassemon <lassemon.pirinen@gmail.com>
2018-08-13 21:13:42 +0300

    Fix database connection into a singleton

--- api/src/database/connection.ts
+++ api/src/database/connection.ts
@@ -1,19 +1,21 @@
 import * as Knex from 'knex';
 
 export class Connection {
-  private knexConnector: Knex;
+  private knexConnector: Knex = null;
 
   constructor() {
-    this.knexConnector = Knex({
-      client: 'pg',
-      debug: process.env.LOG_LEVEL === 'DEBUG',
-      connection: {
-        host: process.env.DATABASE_HOST || 'db',
-        user: process.env.DATABASE_USER || '',
-        password: process.env.DATABASE_PASSWORD || '',
-        database: process.env.DATABASE_SCHEMA || ''
-      }
-    });
+    if (this.knexConnector === null) {
+      this.knexConnector = Knex({
+        client: 'pg',
+        debug: process.env.LOG_LEVEL === 'DEBUG',
+        connection: {
+          host: process.env.DATABASE_HOST || 'db',
+          user: process.env.DATABASE_USER || '',
+          password: process.env.DATABASE_PASSWORD || '',
+          database: process.env.DATABASE_SCHEMA || ''
+        }
+      });
+    }
   }
 
   public getKnex(): Knex {
